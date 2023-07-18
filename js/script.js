9d5454770d715df7d6a6e6aaaf0b79a23d5f3662
Sasha Dube <Sashamicheldube@gmail.com>
2023-07-18 15:34:28 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,175 @@
+// alert('This is a javascript connectivity')
+
+// Variables 
+// Variable is a container or Variables are memory locations which hold values 
+
+//var a = 1;
+
+/* let a = 3; Interger datatype
+let b = "Sasha"; String datatype
+let c = false; boolean 
+*/
+
+
+/*let a = 5;
+let b = 3; 
+
+let result = a + b;
+
+alert(result);*/
+//  alert("the result of 5+ 3 = " + result);
+
+
+
+
+// Conditional Statement 
+// Javascript Datatypes
+
+/*let a = 7;
+let b = 30;
+
+if(a > b){
+console.log("A is higher than B");
+}
+
+else {
+   console.log("A is less than B");
+}*/
+
+// Tenary operation
+/*let a = 7
+let b = 30
+console.log(a > b ? "A is bigger" : "B is bigger");
+
+//  The Switch statement
+/* evaluates an expression, matching the expression's value to a 
+ case clause, and excutes statements associated with that case*/
+
+//   working on a switch 
+
+/*let city = "Benin";
+
+switch(city) {
+    case "Abuja":
+        console.log("Abuja is the federal Capital of Nigeria");
+        break;
+
+    case "Benin":
+        console.log("Benin is the heart of Nigeria");
+        break;
+
+
+    case "Accra":
+        console.log("Accra is the capital of Ghana");
+        break;
+
+
+    default:
+        console.log("Selected city is not availble");
+
+}*/
+
+// ARITHETIC OPERATORS 
+/* 7+7
+7*7
+7-7
+7/7
+let addition = a+b;
+  
+// Equality operators
+=== (equal signs)
+!== ( not equal signs)
+*/
+
+/*EQUALITY OPERATORS
+let a = 11;
+let b = 11;
+if (a === b) {
+    console.log("Same values");
+}
+
+else {
+    console.log("Different values");
+}*/
+
+// UNARY OPERATIORS
+/* ++num
+num++,+
+let year = "1967";
+console.log(++year)
+
+let year = "1967";
+console.log(++year);
+*/
+/* RELATIONAL OPERATORS
+<,>,>=,<=
+if(4>=9){}
+if(num1<=num2)
+
+LOGICAL OPERATORS
+&& ( And sign)
+|| (Or sign)
+if (a < b && b < c){}
+if (a > b || b > c){}
+
+CONDITIONAL OPERATORS
+if
+else 
+?
+switch
+
+ASSIGMENT OPERATORS
++=
+-=
+*=
+=
+
+//  Walking with all the operators 
+//    // Question 
+    //Write a program to find the maximum of three numbers 
+    
+let num1 = 101, num2 = 32,num3 = 57;
+if (num1 > num2 && num1 > num3) {
+    console.log("Num 1 is the highest value");
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log("Num 2 is the highest value");
+}
+else if (num3 > num1 && num3> num2){
+    console.log("Num 3 is the highest value");
+}
+else {
+    console.log("All numbers are the same");
+}*/
+
+// Write a program to find theb following scores in a tertiary institution.
+// If a student got between 75-100 print out >> FIRST CLASS UPPER
+// If a student got between 65-74 print out >> SECOND CLASS UPPER
+// If a student got between 50-64 print out >> SECOND CLASS UPPER
+// If a student got between 41-49 print out >> THIRD CLASS
+// If a student got between 30-40 print out >> PASS
+// ELSE FAIL
+
+let scores = 85;
+let scores =  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
